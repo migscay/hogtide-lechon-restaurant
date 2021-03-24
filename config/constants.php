a08eaad6862abe84bed7e10e35e907c290cada7e
Miguel Vicente O. Santiago <migscay@users.noreply.github.com>
2021-03-24 18:39:18 +1100

    freedb

--- config/constants.php
+++ config/constants.php
@@ -2,10 +2,10 @@
     session_start();
 //by default username=root password=''
     define('SITEURL','http://localhost/food-order/');
-    define('LOCALHOST','localhost');
-    define('DB_USER','root');
-    define('DB_PASSWORD','');
-    define('DB_NAME','food-order');
+    define('LOCALHOST','freedb.tech');
+    define('DB_USER','freedbtech_migscay');
+    define('DB_PASSWORD','MKikay1966');
+    define('DB_NAME','freedbtech_hogtidelechon');
 
     $conn = mysqli_connect(LOCALHOST,DB_USER,DB_PASSWORD) or die(mysqli_error());
     $db_select = mysqli_select_db($conn,DB_NAME) or die(mysqli_error());
